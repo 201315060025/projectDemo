7d07c8158b3c9f22d2bcba34f5fae5785dea1ab6
1392133729 <1392133729@qq.com>
2020-06-21 18:30:19 +0800

    add link

--- data_structure_example/base_suan_fa.py
+++ data_structure_example/base_suan_fa.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 
 def bud_rank():
+    """
+    冒泡排序：两两排序 相邻的两个元素交换位置
+    时间复杂度 最优O（n） 最差是O(n**2)
+    稳定
+    """
     a = [2, 4, 1, 6, 3, 5]
     ln = len(a)
     for i in range(ln):
@@ -16,6 +21,11 @@ def bud_rank():
 
 
 def insert_rank():
+    """
+    插入排序：对未排序的数据，在已排序的数据中从后向前扫描，找到相应的位置插入
+    时间复杂度 最优O（n） 最差是O(n**2)
+    稳定
+    """
     a = [2, 4, 1, 6, 3, 5]
     ln = len(a)
     for i in range(1, ln):
@@ -29,20 +39,33 @@ def insert_rank():
     print("\n"*2)
 
 def select_rank():
+    """
+    选择排序： 在未排序的序列中找出最大（小）值，放到排序序列的起始位置。
+    时间复杂度 最优O(n**2) 最差是O(n**2)
+    不稳定
+    """
     a = [2, 4, 1, 6, 3, 5]
     ln = len(a)
-    for i in range(1, ln):
-        for j in range(1, i):
-            if a[i - 1] > a[j]:
-                tmp = a[j - 1]
-                a[i - 1] = a[j]
-                a[j] = tmp
-    print("insert rank result")
+    for i in range(ln-1):
+        # 声明最小值的索引
+        min_idx = i
+        for j in range(i+1, ln):
+            if a[j] < a[min_idx]:
+                min_idx = j
+
+        if min_idx != i:
+            a[i], a[min_idx] = a[i], a[min_idx]
+
+    print("select rank result")
     print(a)
     print("\n" * 2)
 
 
 def quily_rank():
+    """
+    快速排序： 划分交换排序，通过第一堂数据将要排序的数据分割成独立的两部分。
+
+    """
     a = [2, 4, 1, 6, 3, 5]
 
     def quily_rank_obj(items):
@@ -60,7 +83,12 @@ def quily_rank():
 
 
 def shell_rank():
-    """希尔排序"""
+    """
+    希尔排序: 步长排序， 将数字列在一个表中， 并对每个列进行插入排序， 重复这个过程， 不过每次使用的步长列
+    最优时间复杂度：根据步长序列的不同而不同
+    最坏：O(n**2)
+    不稳定
+    """
     items = [2, 4, 1, 6, 3, 5]
     ln = len(items)
     sep = ln // 2
---
+++ data_structure_example/oneWayList.py
@@ -0,0 +1,114 @@
+class SingleNode(object):
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+class SingleLinkList(object):
+    def __init__(self):
+        self._head = None
+
+    def is_empty(self):
+        return self._head == None
+
+    def length(self):
+        cur = self._head
+        count = 0
+        while cur.next != None:
+            count += 1
+            cur = cur.next
+
+        return count
+
+
+    def tracel(self):
+        """遍历"""
+        cur = self._head
+        while cur.next != None:
+            print(cur.item)
+            cur = cur.next
+        pass
+
+    def add(self, item):
+        """头部添加"""
+        node  = SingleNode(item)
+        node.next = self._head
+        self._head = node
+
+    def append(self, item):
+        """尾部添加"""
+        node = SingleNode(item)
+        # 判断是否为空，
+        if self.is_empty():
+            self._head = node
+            return None
+
+        current_node = self._head
+        while current_node.next != None:
+            current_node = current_node.next
+        current_node.next = node
+
+    def insert(self, pos, item):
+        """
+        指定位置添加
+        1： 验证是否是头部
+        2： 验证是否是尾部
+        """
+        if pos<= 0:
+            self.add(item)
+
+        elif pos > self.length():
+            self.append(item)
+        else:
+            node = SingleNode(item)
+            pre = self._head
+            count = 0
+            while count != pos:
+                count += 1
+                pre = pre.next
+
+            node.next = pre.next
+            pre.next = node
+
+    def remove(self, item):
+        pass
+
+    def search(self, item):
+        """
+        检查节点是否存在
+        """
+        current_node = self._head
+        flag = True
+        while current_node.item != item:
+            if current_node.next == None:
+                flag = False
+                # break
+            current_node = current_node.next
+        return flag
+
+    def hasCycle(self):
+        """
+        判断是否有环
+        判断一个单链表是否有环,
+        可以用 set 存放每一个 节点, 这样每次 访问后把节点丢到这个集合里面.
+        其实 可以遍历这个单链表, 访问过后,
+        如果这个节点 不在 set  里面, 把这个节点放入到 set 集合里面.
+        如果这个节点在  set 里面 , 说明曾经访问过, 所以这个链表有重新 走到了这个节点, 因此一定有环.
+
+        如果链表都走完了, 把所有的节点都放完了. 还是没有重复的节点, 那说明没有环.
+        """
+        nums = set()
+        current_node = self._head
+        flag = True
+        while current_node.next != None:
+            if current_node.item not in nums:
+                nums.add(current_node.item)
+            else:
+                flag = False
+        return flag
+
+
+
+
+
+
+        pass
