3bbaed35ac217645e8934448d415cc9a7b4f674a
bilixin <bilixin@4paradigm.com>
2023-06-23 18:47:20 +0800

    完善爬虫代码

--- AdilCrawler/spiderTiKu/main2.py
+++ AdilCrawler/spiderTiKu/main2.py
@@ -1,11 +1,30 @@
-# coding: utf-9
+# coding: utf-8
 """
 爬取题库的试题
 https://www.tiku.cn/index/index/questions.html?cid=14&cno=1&vid=800023&typeid=600126
 """
-import logging
+import logging, os, sys, pandas as pd, random, time, json
+sys.path.append(os.getcwd())
+from parse_html import ParseHtml
 
-logging.basicConfig(filename="test.log", filemode="w", format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%d-%M-%Y %H:%M:%S"
+# LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+# DATE_FORMAT = "%Y-%d-%m %H:%M:%S"
+#
+# logging.basicConfig(filename='test.log', level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+logger = logging.getLogger('mylogger')
+logger.setLevel(logging.INFO)
+rf_handler = logging.StreamHandler(sys.stderr)  # 默认是sys.stderr
+rf_handler.setLevel(logging.INFO)
+# rf_handler = logging.handlers.TimedRotatingFileHandler('all.log', when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
+rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+
+f_handler = logging.FileHandler('error.log')
+f_handler.setLevel(logging.INFO)
+f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+
+logger.addHandler(rf_handler)
+logger.addHandler(f_handler)
 
 
 class TiKuSpider(object):
@@ -50,29 +69,34 @@ class TiKuSpider(object):
         topic_type = [{
             "name": '应用题', "typeid": 600126
         }]
+        return topic_type
 
-    def get_content_by_page(self, url, page=1):
-        """获取详情"""
-
-
+    def get_analysis(self):
+        """获取解析答案"""
+        self.parser.get_answer_detail()
         pass
+
+    def get_content_by_page(self, url):
+        """获取详情"""
+        # url = "https://www.tiku.cn/index/index/questions.html?cid=14&cno=1&vid=800016&bid=801013&typeid=600126"
+        detail_list = self.parser.get_detail_list(url)
+        detail_data_list = []
+        for detail_one in detail_list:
+            detail_data_list.append(self.parser.get_detail_by_one(detail_one))
+        return detail_data_list
 
     def get_page_list(self, url) -> int:
         """获取总共有多少页"""
-
-        return 1
-
-    def get_detail_by_category(self, category_url):
-        """获取某一个类型下面所有的数据"""
+        return self.parser.get_page(url)
 
 
 
     def main(self):
         """"""
-        base_url = "https://www.tiku.cn/index/index/questions.html?cid=14&cno=1&vid={}&bid={}&typeid={}&page={}"
+        base_url = "https://www.tiku.cn/index/index/questions.html?cid=14&cno=1&vid={}&bid={}&typeid={}"
         # 获取不同类型的版本
         version_list = self.get_version_list()
-        self.logger.info("版本信息：" )
+        self.logger.info("版本信息：")
         self.logger.info(version_list)
         # 获取不同的课本
         course_list = self.get_course_list()
@@ -81,20 +105,85 @@ class TiKuSpider(object):
         topic_type_list = self.get_topic_type()
         self.logger.info("题目类型：")
         self.logger.info(topic_type_list)
-        result = {}
+
+        """
+        {'edition':'人教版','book':'三年级上册','level':'0.72', 'original_text':'小明为玉树灾区的小朋友捐了55本故事书，46本连环画，一共是多少本书?','ans':'','resolution':'55+46=101(本)' ,'analysis':''}
+        """
+        # step1:
+        """
+        基本url, 最大页码数目，版本， book， 
+        """
+        step_result = []
+        version_list = []
         for version in version_list:
+            v_id, v_name = version["vid"], version["name"]
             for course in course_list:
+                c_id, c_name = course["bid"], course["name"]
                 for topic in topic_type_list:
-                    cur_url = base_url.format(str(version), str(course), str(topic), '1')
+                    t_id, t_name = topic["typeid"], topic["name"]
+                    cur_url = base_url.format(str(v_id), str(c_id), str(t_id))
+                    self.logger.info("start: " + cur_url)
+                    page_num = self.get_page_list(cur_url)
+                    step_result.append({
+                        "v_id": v_id,
+                        'v_name': v_name,
+                        "c_id": c_id,
+                        "c_name": c_name,
+                        't_id': t_id,
+                        "t_name": t_name,
+                        "max_page": page_num,
+                        "url": cur_url
+                    })
+        # print(step_result)
+        # 把第一步结果保存保存
+        # df = pd.DataFrame(data=step_result)
+        # df.to_csv("step_result.csv", index=False)
+        df = pd.read_csv("step_result.csv")
+        cl_list = df.columns.tolist()
+        data_list = df.values.tolist()
+        step_result = [dict(zip(cl_list, i)) for i in data_list]
 
-
-
-
-        pass
+        # step 2
+        """
+        基础url ,页码数字（1， 2），  
+        """
+        step2_result_ff = "step2_result.csv"
+        if os.path.exists(step2_result_ff):
+            his_df = pd.read_csv(step2_result_ff)
+        else:
+            his_df = pd.DataFrame({"page": [], "base_url": [], "cur_url": [], "page_context": []})
+        history_cur_url_list = list(his_df["cur_url"])
+        step2_result = []
+        number = 0
+        for stp in step_result:
+            max_page, base_url = stp["max_page"], stp["url"]
+            for cur_page in range(1, max_page+1):
+                cur_url = base_url + "&page=" + str(cur_page)
+                if cur_url not in history_cur_url_list:
+                    self.logger.info("{} 已经存在".format(cur_url))
+                    continue
+                cur_result = self.get_content_by_page(cur_url)
+                if len(cur_result) == 0:
+                    self.logger.error("请求被拒绝， 获取数据为空")
+                    continue
+                number += 1
+                self.logger.info("{} 当前url:{}, 获取结果：{} ".format(str(number), cur_url, len(cur_result)))
+                step2_result.append({
+                    "page": cur_page,
+                    "base_url": base_url,
+                    "cur_url": cur_url,
+                    "page_context": json.dumps(cur_result)
+                })
+                # 获取所有也内容
+                time.sleep(random.random())
+                df = pd.DataFrame(data=step2_result)
+                mode = "a" if len(history_cur_url_list) == 0 else 'w'
+                df.to_csv("step2_result.csv", mode=mode, index=False)
 
 
 if __name__ == "__main__":
-    obj = TiKuSpider("https://www.tiku.cn/index/index/questions.html", logger=)
+    parse = ParseHtml(logger=logger)
+    obj = TiKuSpider("https://www.tiku.cn/index/index/questions.html", logger=logger, parser=parse)
     obj.main()
 
 
--- AdilCrawler/spiderTiKu/parse_html.py
+++ AdilCrawler/spiderTiKu/parse_html.py
@@ -36,16 +36,31 @@ class ParseHtml(object):
         try:
             diff_value = one_detail_element.div.span.a.text
             cur_id = [i for i in one_detail_element.children if not i.find('div')][-1].attrs["id"]
-            context = one_detail_element.section.text
-            return {"diff_value": diff_value, "cur_id": cur_id, 'context': context}
+            img_context_list = one_detail_element.section.find_all("img")
+            img_str = ""
+            if img_context_list:
+                img_src_list = [i.attrs["src"] for i in img_context_list]
+                img_str = "\n".join(img_src_list)
+                img_str = "图片内容：" + img_str
+            context = one_detail_element.section.text + img_str
+            analysis = self.get_answer_detail(cur_id.replace("ana", ""))
+            return {"diff_value": diff_value, "cur_id": cur_id, 'context': context, "analysis": analysis}
         except:
             return {}
 
-    def get_answer_detail(self, id):
+    def get_answer_detail(self, id) -> dict:
         """获取当前的详情"""
-
-        pass
-
+        url = "https://www.tiku.cn/tikuapi/question/anylysis/"
+        header = {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "Cookie": "PHPSESSID=2mnj85ehv3bsq08qr8nlvcamn7; Hm_lvt_02f32149c7ea90d0cd47ed89025e457c=1686917647; think_var=zh-cn; uid=54681; token=e44a179d-9d50-45df-bfed-758eb5b1fe76; Hm_lpvt_02f32149c7ea90d0cd47ed89025e457c=1687336251",
+            "Origin": "https://www.tiku.cn",
+            "Referer": "https://www.tiku.cn/index/index/questions.html?cid=14&cno=1&vid=800016&bid=800110",
+            "X-Requested-With": "XMLHttpRequest"
+        }
+        res = requests.post(url, headers=header, data={"id": id})
+        return json.loads(res.text)
 
 
 import logging
@@ -66,11 +81,13 @@ def get_detail_list_test():
     print(res)
 
 
-    pass
+def get_answer_detail_test():
+    id = "3012447"
+    res = parse.get_answer_detail(id)
 
 
 if __name__ == '__main__':
-    get_detail_list_test()
+    get_answer_detail_test()
 
 
 
